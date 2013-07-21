f496bdefe0679d390515828d573db2a2adddd9a1
otiai10 <otiai10@gmail.com>
2013-07-21 15:35:18 +0900

    make convert version

--- system/util.py
+++ system/util.py
@@ -5,4 +5,7 @@ def get_file_name(file__, contain_file_extension=False):
   return file__.split('/')[-1].split('.')[0]
 
 def convert_twitter_format(tw):
+  return _convert_v1(tw)
+
+def _convert_v1(tw):
   return tw
