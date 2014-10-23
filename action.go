31d699f5c5c5ff7196e7653f615e942417643475
otiai10 <otiai10@gmail.com>
2014-10-23 21:25:38 +0900

    Fix a bit

--- action.go
+++ action.go
@@ -4,31 +4,38 @@ import "fmt"
 import "reflect"
 import "github.com/otiai10/twistream"
 
-type IAction interface {
+// MatchExecuter ...
+type MatchExecuter interface {
 	Match(status Status) bool
 	Execute(status Status)
 }
 
-var actionRegistry = []IAction{}
+var actionRegistry = []MatchExecuter{}
 
+// Action ...
 type Action struct {
 	TL *twistream.Timeline
 }
 
+// Match ...
 func (c *Action) Match(status Status) bool {
 	return true
 }
+
+// Execute ...
 func (c *Action) Execute(status Status) {
 	// hoge
 }
 
+// Tweet ...
 func (c *Action) Tweet(status Status) error {
 	return c.TL.Tweet(status.Status)
 }
 
-func AppendAction(action IAction) (e error) {
-	if _, ok := action.(IAction); !ok {
-		return fmt.Errorf("%s does not implement IAction", reflect.TypeOf(action).String())
+// AppendAction ...
+func AppendAction(action MatchExecuter) (e error) {
+	if _, ok := action.(MatchExecuter); !ok {
+		return fmt.Errorf("%s does not implement MatchExecutor", reflect.TypeOf(action).String())
 	}
 	actionRegistry = append(actionRegistry, action)
 	return
